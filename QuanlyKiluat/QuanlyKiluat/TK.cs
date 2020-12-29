5e06cd318f13d65e433e9d851318694c1cf32196
NguyenQuocKhanh <44228638+kaanblack09@users.noreply.github.com>
2020-12-29 07:34:32 +0700

    t
    t

--- QuanlyKiluat/QuanlyKiluat/TK.cs
+++ QuanlyKiluat/QuanlyKiluat/TK.cs
@@ -20,9 +20,18 @@ namespace QuanlyKiluat
         private void TK_Load(object sender, EventArgs e)
         {
             var db = new Quanlykiluat1Entities();
-            chart1.DataSource = db.getPLKL_DaiDoi_2().ToList();
+            var table = db.getPLRL_DaiDoi_2().ToList();
+            chart1.DataSource = table;
             chart1.Series[0].XValueMember = "TenPLRL";
             chart1.Series[0].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            chart1.Series[0].YValueMembers = "Soluong";
+            chart1.Series[0].YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+
+            chart2.DataSource = db.getPLKL_DaiDoi();
+            chart2.Series[0].XValueMember = "TenPLKL";
+            chart2.Series[0].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            chart2.Series[0].YValueMembers = "Soluong";
+            chart2.Series[0].YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
         }
     }
 }
